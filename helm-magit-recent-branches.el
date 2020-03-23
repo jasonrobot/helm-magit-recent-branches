3025313ed496d8d5d2eb4185e57b35895a950be1
Jason Howell <jhowell@ontraport.com>
2020-03-23 00:37:07 -0700

    Cleaning up code, bringing in s and dash for nicer features.

--- helm-magit-recent-branches.el
+++ helm-magit-recent-branches.el
@@ -6,53 +6,62 @@
 
 ;;; Code:
 
+(require 'dash)
+(require 's)
 (require 'subr-x)
 
-(defun build-helm-candidates (git-output-lines)
-  "Make the helm candidates for recent-branches from GIT-OUTPUT-LINES."
-  (mapcar
-   (lambda (line)
-     (let* ((branch-name (propertize (elt line 0)
-                                     'face 'magit-branch-remote))
-            (commit-message (elt line 1))
-            (commit-date (propertize (elt line 2)
-                                     'face 'magit-log-date))
-            (message-length (- (window-body-width)
-                               (length branch-name)
-                               (length commit-date)
-                               ;; length of " - " twice, plus some for the window width
-                               12)))
-       (format "%s - %s - %s"
-               branch-name
-               (if (> (length commit-message) message-length)
-                   (format "%s..." (seq-take commit-message (- message-length 3)))
-                 commit-message)
-               commit-date)))
-   git-output-lines))
+(defun build-helm-candidate (line)
+  "Make a helm candidate for recent-branches from LINE."
+  (let* ((branch-name (propertize (elt line 0)
+                                  'face 'magit-branch-remote))
+         (commit-message (elt line 1))
+         (commit-date (propertize (elt line 2)
+                                  'face 'magit-log-date))
+         (message-length (- (window-body-width)
+                            (length branch-name)
+                            (length commit-date)
+                            ;; length of " - " twice, plus some for the window width
+                            12)))
+    (format "%s - %s - %s"
+            branch-name
+            (if (> (length commit-message) message-length)
+                (format "%s..." (seq-take commit-message (- message-length 3)))
+              commit-message)
+            commit-date)))
+
+(defun remove-current-branch-marker-column (line)
+  "Our git output will include a column in each LINE that we use to filter out the current branch.  After we remove it, it will be empty in all lines, so remove the first element."
+  (--map (seq-drop (split-string it ";") 1) line))
+
+(defun remove-current-branch-from-lines (lines)
+  "Remove the current branch from LINES."
+  (--reject (s-equals? "*" (car it)) lines))
 
 (defun helm-magit-recent-branches ()
   "Get recent git branches."
   (interactive)
   (let* ((git-output (shell-command-to-string "git for-each-ref --sort='-committerdate' refs/heads/ --format='%(HEAD);%(refname:short);%(contents:subject);%(committerdate:relative)' | head -n 50"))
          ;; trim, split the command output into lines, split the lines into tokens, remove the current branch
-         (git-output-lines (mapcar (lambda (line) (seq-drop (split-string line ";") 1))
-                                   (seq-filter (lambda (line) (not (= (string-to-char "*") (elt line 0))))
-                                               (split-string (string-trim git-output) "\n"))))
+         (git-output-lines (-> git-output
+                               (s-trim)
+                               (s-lines)
+                               (remove-current-branch-from-lines)
+                               (remove-current-branch-marker-column)))
          ;; get the list of the commit hashes
-         (branch-names (mapcar (lambda (line) (elt line 0)) git-output-lines))
+         (branch-names (--map 'car git-output-lines))
          ;; format that into a nice thing we can show the user - we'll be matching of the index later
-         (helm-candidates (build-helm-candidates git-output-lines))
+         (helm-candidates (-map 'build-helm-candidate git-output-lines))
          ;; do the helm thing and get the result
-         (helm-selection (helm :sources (helm-build-sync-source "recent branches"
-                                          :candidates helm-candidates
-                                          :fuzzy-match nil)
-                               :buffer "*magit helm recent branches*"))
+         (helm-selection (helm :sources
+                               (helm-build-sync-source "recent branches"
+                                 :candidates helm-candidates
+                                 :fuzzy-match nil)
+                               :buffer "*magit recent branches*"))
          ;; the index of their helm selection is the index of the commit hash we want
-         (selected-commit (elt branch-names (cl-position helm-selection helm-candidates :test 'string=))))
+         (selected-commit (elt branch-names (--find-index (s-equals? it helm-selection) helm-candidates))))
     ;; checkout that commit
     (magit-checkout selected-commit)))
 
 (provide 'magit-helm-recent-branches)
 ;;; magit-helm-recent-branches.el ends here
 
-; magit-branch-remote -  - magit-log-date
